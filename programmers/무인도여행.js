7ecd50a267fefd3d30da9a208360f4be402b76ae
dlzagu <eodnsdlekd@naver.com>
2023-02-03 23:35:09 +0900

    20230203:[PGS]무인도 여행

--- programmers/무인도여행.js
+++ programmers/무인도여행.js
@@ -1,29 +1,32 @@
 function solution(maps) {
-  maps = maps.map((a) => a.split("")).map((a) => a.map(Number));
   const col = maps[0].length;
   const row = maps.length;
+  const answer = [];
   const direction = [
     [0, 1],
     [0, -1],
     [-1, 0],
     [1, 0],
   ];
-  const visited = Array(row).fill(Array(col).fill(0));
+
+  const visited = Array.from(Array(maps.length), () =>
+    Array(maps[0].length).fill(false)
+  );
   const check = (i, j) => {
-    visited[i][j] = 1;
-    let cnt = maps[i][j];
+    visited[i][j] = true;
+    let cnt = Number(maps[i][j]);
     for (let dir of direction) {
       const xx = dir[1] + j;
       const yy = dir[0] + i;
+
       if (
         xx >= 0 &&
         yy >= 0 &&
         xx < col &&
         yy < row &&
-        maps[yy][xx] &&
+        maps[yy][xx] !== "X" &&
         !visited[yy][xx]
       ) {
-        console.log(maps[yy][xx]);
         cnt += check(yy, xx);
       }
     }
@@ -32,11 +35,11 @@ function solution(maps) {
 
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
-      if (maps[i][j] && !visited[i][j]) {
-        console.log(check(i, j));
+      if (maps[i][j] !== "X" && !visited[i][j]) {
+        answer.push(check(i, j));
       }
     }
   }
 
-  return;
+  return answer.length ? answer.sort((a, b) => a - b) : [-1];
 }
