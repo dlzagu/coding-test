2861a3b7fac2a817f55da080463f9f76eb38e7ea
dlzagu <eodnsdlekd@naver.com>
2023-02-06 20:44:04 +0900

    20230206:[PGS]스킬트리  (다른풀이)

--- programmers/스킬트리.js
+++ programmers/스킬트리.js
@@ -1,25 +1,13 @@
 function solution(skill, skill_trees) {
-  var answer = 0;
-  const orders = {};
-  for (let i = 0; i < skill.length; i++) {
-    orders[skill[i]] = i;
-  }
-  while (skill_trees.length) {
-    const skillString = skill_trees.shift();
-    answer += check(skillString, orders);
-  }
-  return answer;
-}
-const check = (string, orders) => {
-  const visited = [];
-  for (let i = 0; i < string.length; i++) {
-    const order = orders[string[i]];
-    if (order !== undefined) {
-      for (let j = 0; j < order; j++) {
-        if (!visited.includes(j)) return 0;
-      }
-      visited.push(order);
+  function isCorrect(n) {
+    let test = skill.split("");
+    for (var i = 0; i < n.length; i++) {
+      if (!skill.includes(n[i])) continue;
+      if (n[i] === test.shift()) continue;
+      return false;
     }
+    return true;
   }
-  return 1;
-};
+
+  return skill_trees.filter(isCorrect).length;
+}
