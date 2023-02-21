d9d51c3cfd58d7e7274ac44db25ede32b62c7ed0
dlzagu <eodnsdlekd@naver.com>
2023-02-21 14:54:05 +0900

    20230221:[PGS]카드뭉치

---
+++ programmers/카드뭉치.js
@@ -0,0 +1,11 @@
+function solution(cards1, cards2, goal) {
+  while (goal.length) {
+    const word = goal.shift();
+    if (cards1[0] === word) {
+      cards1.shift();
+    } else if (cards2[0] === word) {
+      cards2.shift();
+    } else return "No";
+  }
+  return "Yes";
+}
